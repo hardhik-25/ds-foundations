57e1ceda0af95601b7683115b8404dc02cc78758
Hardhik <dhangerhardhik25@gmail.com>
2026-07-07 23:28:58 +0530

    feat: day 2 revision - truesum written from memory

--- day02.py
+++ day02.py
@@ -24,3 +24,14 @@ print(truesum(nums,target))
 nums = [1,4,6,8]
 target = 3
 print(truesum(nums,target))
+
+#revision
+def TrueSum(nums, target):
+    for i in range(len(nums)):
+        for j in range(i+1, len(nums)):
+            if nums[i] + nums[j] == target:
+                return True
+    return False
+
+print(TrueSum([1, 4, 6, 8], 10))
+print(TrueSum([1, 4, 6, 8], 3))
