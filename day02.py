4639cd5d0f56548f6a25a4b892b0a582e42c092c
Hardhik <dhangerhardhik25@gmail.com>
2026-07-07 11:09:20 +0530

    feat: day 2 - two sum solved independently

---
+++ day02.py
@@ -0,0 +1,26 @@
+
+def twosum(nums, target):
+    for i in range(len(nums)):
+       for j in range(i+1,len(nums)): 
+            if nums[i] + nums[j] == target:
+                return [i,j]
+nums = [3,2,4]
+target = 6
+print(twosum(nums, target))
+
+
+# Write a function that takes a list of numbers and a target. Return True if any 
+# two numbers in the list add up to the target, and False if they don't.
+def truesum(nums,target):
+    for i in range(len(nums)):
+        for j in range(i+1,len(nums)):
+            if nums[i] + nums[j] == target:
+                return True
+    return False
+nums = [1,4,6,8]
+target=10
+print(truesum(nums,target))
+
+nums = [1,4,6,8]
+target = 3
+print(truesum(nums,target))
